8928533ea93f5b23c8d7d2fe1c9a48fafff79369
W. Wesley Monroe <ww.monroe@gmail.com>
2015-07-19 14:56:32 -0500

    initial checkin

---
+++ public/javascripts/main.js
@@ -0,0 +1,71 @@
+angular.module("newApp", ["ngRoute"])
+.config(function($routeProvider){
+	$routeProvider.when("/courses", {
+		controller : "courses",
+		templateUrl : "/templates/courses.html"
+	})
+	.when("/courses/:id", {
+		controller: "modules",
+		templateUrl : "/templates/modules.html"
+	})
+	.otherwise("/courses");
+})
+
+.controller("courses", function($http, $scope){
+	$http.get('/api/canvas/courses')	
+	.success(function (courses) {
+		$scope.courses = courses;
+	})
+})
+
+.controller("modules", function($http, $scope, $routeParams){
+	console.log($routeParams.id);
+	$http.get('/api/canvas/courses/'+ $routeParams.id + "/modules")
+	.success(function (modules, status, headers) {
+		$scope.modules = modules;
+	})
+})
+
+
+
+
+
+// $(function () {
+// 	console.log('started');
+
+// 	var accessToken = "7~PBUEpEjESZrm0w9l91WZKk5gOtAggzGCN3s29hkCtnl1cdq6GLLY4Hocrr9iHSr0";
+// 	var endpoint = 'https://canvas.instructure.com/api/v1/courses';
+
+// 	window.jsonCallback = function (res) {
+// 		console.log('JSONP:', arguments);
+// 	};
+
+// 	function updateDom (res) {
+// 		$('#content > p').html(res);
+// 	}
+
+// 	function displayMessage (message) {
+// 		$('#messages').append('<p>' + message + '</p>');
+// 	}
+
+// 	$(function () {
+
+// 		displayMessage('Fetching courses');
+
+// 		$.ajax({
+// 	    url: '/api/courses',
+// 	    type: 'GET',
+// 			success: function (res) {
+// 				updateDom(res);
+// 				displayMessage('Success!');
+// 			},
+// 			error: function (xhr, status, error) {
+// 				updateDom('');
+// 				displayMessage(status + '; ' + error)
+// 			}
+// 		});
+
+// 	});
+
+
+// });
