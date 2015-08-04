96d0987bad9c82b9e04f43d5a2d857c2f3d4cf93
W. Wesley Monroe <ww.monroe@gmail.com>
2015-08-04 04:43:40 -0500

    Removed jade, added in lessonItems, and refatored to use services in main.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,28 +8,96 @@ angular.module("newApp", ["ngRoute"])
 		controller: "modules",
 		templateUrl : "/templates/modules.html"
 	})
+	.when("/courses/:id/module/:id2", {
+		controller: "lessonItems",
+		templateUrl : "/templates/lessonItems.html"
+	})
 	.otherwise("/courses");
 })
 
-.controller("courses", function($http, $scope){
-	$http.get('/api/canvas/courses')	
+.controller("courses", function($scope, courses){
+	courses.get()
 	.success(function (courses) {
 		$scope.courses = courses;
 	})
 })
 
-.controller("modules", function($http, $scope, $routeParams){
-	console.log($routeParams.id);
-	$http.get('/api/canvas/courses/'+ $routeParams.id + "/modules")
+.controller("modules", function(modules, $scope, $routeParams){
+	modules.get($routeParams.id)
 	.success(function (modules, status, headers) {
 		$scope.modules = modules;
+		$scope.course = $routeParams.id;
 	})
 })
 
+.controller("lessonItems", function(moduleItems, $scope, $routeParams){
+	console.log($routeParams.id);
+	$scope.loadPage = function(pageNum) {
+
+		moduleItems.get($routeParams.id, $routeParams.id2, pageNum)
+		.success(function (lessonItems, status, headers) {
+			var pgs = makePageMap(headers('link'));
+
+			console.log(pgs);
+			$scope.pages = pgs;
+			$scope.pageNumbers = createArray(pgs.last);
+			$scope.lessonItems = lessonItems;
+		})
+	}
+	$scope.loadPage(1);
+})
+
+.factory("courses", function($http) {
+	return {
+		url: '/api/canvas/courses',
+		get: function() {
+			return $http.get(this.url)	
+		}
+	};
+})
+
+.factory("modules", function($http, courses) {
+	return {
+		url: function(courseID) {
+			return courses.url + '/' + courseID + '/modules'
+		},
+		get: function(courseID) {
+			return $http.get(this.url(courseID));	
+		}
+	};
+})
+
+.factory("moduleItems", function($http, modules) {
+	return {
+		url: function(courseID, moduleID) {
+			return modules.url(courseID) + '/' + moduleID + "/items";
+		},
+		get: function(courseID, moduleID, page) {
+			return $http.get(this.url(courseID, moduleID) + '?page=' + page);	
+		}
+	};
+})
+
+function createArray(count) {
+	var array = [];
+	for (var i = 0; i < count; i++) {
+		array[i] = i+1;
+	};
+	return array;
+}
 
 
+function makePageMap(headers) {
+	var pageObjs = {};
+	var pageArr = headers.split(',').map(function(str) {
+		return str.match(/page=(\d+).*rel="(\w+)/);
+	});
+	pageArr.forEach(function(pageMatchs) {
+		pageObjs[pageMatchs[2]] = pageMatchs[1];
+	});
 
-
+	return pageObjs;
+}
 // $(function () {
 // 	console.log('started');
 
